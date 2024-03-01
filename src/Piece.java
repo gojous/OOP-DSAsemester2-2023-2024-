69f05c477c535fec34b9724ad60755f17231d521
Ian31 <hoangphuc3612@gmail.com>
2024-03-02 00:48:16 +0700

    Creating tile and piece abstract

---
+++ src/Piece.java
@@ -0,0 +1,26 @@
+public class Piece {
+    public enum PieceType{
+        PAWN,
+        KNIGHT,
+        BISHOP,
+        ROOK,
+        QUEEN,
+        KING
+    }
+    public enum PieceColor{
+        WHITE,
+        BLACK
+    }
+    private final PieceType pieceType;
+    private final PieceColor pieceColor;
+    Piece(PieceType pieceType,PieceColor pieceColor){
+        this.pieceType=pieceType;
+        this.pieceColor=pieceColor;
+    }
+    public PieceType getPieceType(){
+        return this.pieceType;
+    }
+    public PieceColor getPieceColor(){
+        return this.pieceColor;
+    }
+}
---
+++ src/main/java/com/chess/Tile.java
@@ -0,0 +1,39 @@
+public abstract class Tile{
+    int tileCoordinate;
+    Tile(int tileCoordinate){
+        this.tileCoordinate=tileCoordinate;
+    }
+    public abstract boolean isTileTaken();
+
+    public abstract Piece getPiece();
+    public static final class EmptyTile extends Tile{
+        EmptyTile(int coordinate){
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileTaken(){
+            return false;
+        }
+        @Override
+        public Piece getPiece(){
+            return null;
+        }
+    
+    }
+    public static final class TakenTile extends Tile{
+        Piece pieceOnTile;
+        TakenTile(int coordinate,Piece pieceOnTile){
+            super(tileoordinate);
+            this.pieceOnTile=pieceOnTile;
+        }
+        @Override
+        public boolean isTileTaken(){
+            return true;
+        }
+        @Override
+        public Piece getPiece(){
+            return this.pieceOnTile;
+        }
+    }
+}
